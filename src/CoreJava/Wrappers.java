12bc3d039c85475e9a7703cf127e0e1c19bef69f
aditya.malani <adisai123@gmail.com>
2018-05-16 23:08:56 +0530

    Wrapper

---
+++ src/CoreJava/Wrappers.java
@@ -0,0 +1,19 @@
+package CoreJava;
+
+/**
+ * 
+ * @author aditya.malpani
+ *
+ */
+public class Wrappers
+{
+
+    public static void main(String[] args)
+    {
+        Integer k = new Integer(10);
+        Integer i = 10;
+        Integer j = 10;
+        System.out.println(i == j);
+        System.out.println(k == j);
+    }
+}
