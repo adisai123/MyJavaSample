4cd8b50f0833e65709ed0d7259f4e02bbb6874c4
aditya.malani <adisai123@gmail.com>
2018-06-01 11:52:30 +0530

    getWeekly

---
+++ src/java8New/DayOfWeekExample1.java
@@ -0,0 +1,20 @@
+package java8New;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+
+public class DayOfWeekExample1
+{
+
+    public static void main(String[] args)
+    {
+        // LocalDate localDate = LocalDate.of(2016, 03, 31);
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+        DayOfWeek dayOfWeek = DayOfWeek.of(2);
+        localDate = localDate.with(TemporalAdjusters.next(dayOfWeek));
+        System.out.println(localDate);
+    }
+}
